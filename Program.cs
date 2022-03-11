751f83568324319b379a81d91ae8cb814ec19c83
Alex Roberson <alexroberson99@gmail.com>
2022-03-11 18:43:12 -0500

    Creating c# file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AlexIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hi, Enter your name below;");
+            var teacherName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {teacherName}! How are you today?");
+        }
+    }
+}
